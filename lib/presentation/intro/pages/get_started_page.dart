dddb901ad8aadee72267f924a01f889240d50125
Phanindra-Nelavalli <nelavalliphanindra4@gmail.com>
2025-05-15 17:19:10 +0530

    IntoPage

---
+++ lib/presentation/intro/pages/get_started_page.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:spotify/common/widgets/basic_app_button.dart';
+import 'package:spotify/core/configs/assets/app_images.dart';
+import 'package:spotify/core/configs/assets/app_vectors.dart';
+import 'package:spotify/core/configs/theme/app_colors.dart';
+
+class GetStartedPage extends StatelessWidget {
+  const GetStartedPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 60, horizontal: 40),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.fill,
+                image: AssetImage(AppImages.introBG),
+              ),
+            ),
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: SvgPicture.asset(AppVectors.logo),
+                ),
+                Spacer(),
+                Text(
+                  "Enjoy Listening To Music",
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(height: 21),
+                Text(
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sagittis enim purus sed phasellus. Cursus ornare id scelerisque aliquam.",
+                  style: TextStyle(fontSize: 17, color: AppColors.grey),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 21),
+                BasicAppButton(onPressed: () {}, title: "Get Started"),
+              ],
+            ),
+          ),
+          Container(color: Colors.black.withOpacity(0.15)),
+        ],
+      ),
+    );
+  }
+}
